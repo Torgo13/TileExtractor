abd6eee9d5cc39527d7213e5773bedc7911f79a6
Torgo13 <t.d.walker42@gmail.com>
2023-05-28 21:29:45 +0100

    Initialised CMake project

---
+++ TileExtractor.cpp
@@ -0,0 +1,12 @@
+﻿// TileExtractor.cpp : Defines the entry point for the application.
+//
+
+#include "TileExtractor.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
